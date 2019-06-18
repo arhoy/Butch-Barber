7a2c1c1da067765d8b4ffd2dd3827d23408bddd8
aquasar <arhoy@ualberta.ca>
2019-06-17 18:59:22 -0600

    asdfsaf

---
+++ build/precache-manifest.dfeeb47a9a9990ac7812778b61e3d4b2.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = [
+  {
+    "revision": "336d7629e57340c2bcc5e7bdba306443",
+    "url": "/static/media/bg2.336d7629.png"
+  },
+  {
+    "revision": "137dcf3b3c76561f2e56",
+    "url": "/static/css/main.3ccb4bc5.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "e6e4be89a467c5d28515",
+    "url": "/static/js/2.e6e4be89.chunk.js"
+  },
+  {
+    "revision": "c2cea7c5613d8a948cee95f0383f9ed4",
+    "url": "/static/media/card-blog4.c2cea7c5.jpg"
+  },
+  {
+    "revision": "7a51791653cbe4106623ad80273681a6",
+    "url": "/static/media/barber1.7a517916.jpg"
+  },
+  {
+    "revision": "7b343fcf72981f8475ccad9eaf5558b7",
+    "url": "/static/media/barber2.7b343fcf.jpg"
+  },
+  {
+    "revision": "a978c41d58f92b327a696515930e97c0",
+    "url": "/static/media/barber3.a978c41d.jpg"
+  },
+  {
+    "revision": "b57d6529d7bf206638c40c075511f89d",
+    "url": "/static/media/office2.b57d6529.jpg"
+  },
+  {
+    "revision": "c1870d44e26783b3ef3b0f4a0273c93b",
+    "url": "/static/media/office1.c1870d44.jpg"
+  },
+  {
+    "revision": "7cff6d6615f50f957a667f1fcf74d866",
+    "url": "/static/media/office2.7cff6d66.jpg"
+  },
+  {
+    "revision": "9ab1b882a542b6daddcc8950b2c31fda",
+    "url": "/static/media/office3.9ab1b882.jpg"
+  },
+  {
+    "revision": "baf4c51dee3076b3b0b57be6f9b1a65a",
+    "url": "/static/media/office4.baf4c51d.jpg"
+  },
+  {
+    "revision": "2f540f8700fbc2ee535789e2f603d461",
+    "url": "/static/media/office5.2f540f87.jpg"
+  },
+  {
+    "revision": "58a713c73886bac25ac6aedcbb13de5a",
+    "url": "/static/media/city.58a713c7.jpg"
+  },
+  {
+    "revision": "4fc03597a5af468b668d84c3c9bf319c",
+    "url": "/static/media/bg.4fc03597.png"
+  },
+  {
+    "revision": "137dcf3b3c76561f2e56",
+    "url": "/static/js/main.137dcf3b.chunk.js"
+  },
+  {
+    "revision": "1e0b91ff1f579473090bd1aeae842504",
+    "url": "/static/media/bg3.1e0b91ff.png"
+  },
+  {
+    "revision": "57aed57506861e704b703c5e65f63535",
+    "url": "/static/media/bg_refer_friend.57aed575.png"
+  },
+  {
+    "revision": "84e67f003ad082123534b86c0a7ebc22",
+    "url": "/static/media/dg2.84e67f00.jpg"
+  },
+  {
+    "revision": "3122abf4a3e1067926c08dee7684522d",
+    "url": "/static/media/card-profile1-square.3122abf4.jpg"
+  },
+  {
+    "revision": "788e3d36e8c4d4120daeee6e57e7e85f",
+    "url": "/static/media/card-profile2-square.788e3d36.jpg"
+  },
+  {
+    "revision": "101b9dc3484755250ff3de71d22af8e1",
+    "url": "/static/media/marc.101b9dc3.jpg"
+  },
+  {
+    "revision": "013b04881a6177ff0648349533ef0447",
+    "url": "/static/media/bg7.013b0488.jpg"
+  },
+  {
+    "revision": "5b7c1cad66a2efdbf3604615cc9d7d1c",
+    "url": "/static/media/clint-mckoy.5b7c1cad.jpg"
+  },
+  {
+    "revision": "8cfdd67a8ad34bc88343e1889d7b7255",
+    "url": "/static/media/bg8.8cfdd67a.jpg"
+  },
+  {
+    "revision": "1a1649171cdecd24e7a16ea1340077df",
+    "url": "/static/media/card-profile4-square.1a164917.jpg"
+  },
+  {
+    "revision": "1f1f4900f3bc166b75f8235c2b89864f",
+    "url": "/static/media/card-profile6-square.1f1f4900.jpg"
+  },
+  {
+    "revision": "5a0e395b3b76565583339619ca3110f4",
+    "url": "/static/media/kendall.5a0e395b.jpg"
+  },
+  {
+    "revision": "b23f720528371939315c0df5fc90c150",
+    "url": "/static/media/christian.b23f7205.jpg"
+  },
+  {
+    "revision": "d1571425406887c3c26c4cef0bc2c6d3",
+    "url": "/static/media/bg5.d1571425.jpg"
+  },
+  {
+    "revision": "d1618cfadd9a0b1537e4e2dfc98c6a9f",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/butch-barber/precache-manifest.0259df038f2b79be3549a8d2a6e12b56.js"
+  "/precache-manifest.dfeeb47a9a9990ac7812778b61e3d4b2.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/butch-barber/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
